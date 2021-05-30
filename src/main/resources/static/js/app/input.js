6b52dd67dff4c22448b5e762c252f3fa1a050e19
kimtaejun <kim_taejun@naver.com>
2021-05-30 22:31:02 +0900

    href 오류 수정

--- src/main/resources/static/js/app/input.js
+++ src/main/resources/static/js/app/input.js
@@ -32,7 +32,7 @@ var main = {
         var fileSize = formData.get("image").size;
 
         //size check
-        if(fileSize <10485760){
+        if(fileSize < 10485760){
             $.ajax({
                 type: 'POST',
                 url: '/api/result/inputView',
